5191a6c07e73a9d8a79f216e2730ff41c75d7da6
cattslmao <cattttttslmao@gmail.com>
2023-11-05 01:06:51 -0500

    Initial Commit

---
+++ src/client/java/io/github/cattslmao/bedrockparity/BedrockParityClient.java
@@ -0,0 +1,10 @@
+package io.github.cattslmao.bedrockparity;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BedrockParityClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
