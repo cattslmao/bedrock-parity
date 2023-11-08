6d663a481fa37e190190131d8fd0df6eabd824d8
cattslmao <cattttttslmao@gmail.com>
2023-11-07 19:31:01 -0600

    Make CakeBurp Server

---
+++ src/main/java/io/github/cattslmao/bedrockparity/mixin/CakeBurp.java
@@ -0,0 +1,26 @@
+package io.github.cattslmao.bedrockparity.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CakeBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CakeBlock.class)
+public class CakeBurp {
+    @Inject(method = "tryEat", at=@At("RETURN"))
+    private static void injected(WorldAccess worldAccess, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfoReturnable<ActionResult> cir) {
+        if (cir.getReturnValue() == ActionResult.SUCCESS) {
+            World world = (World) worldAccess;
+            world.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.ENTITY_PLAYER_BURP, SoundCategory.PLAYERS, 0.5F, world.random.nextFloat() * 0.1F + 0.9F);
+        }
+    }
+}
