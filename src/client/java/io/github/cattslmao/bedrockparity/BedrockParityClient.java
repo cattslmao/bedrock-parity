32822cb30f5a2c51ad0dc04e31475f5d51ceb4ff
sonopsa <32681149+sonopsa@users.noreply.github.com>
2023-11-29 16:46:56 -0800

    update mod version and make client mixins json work

--- src/client/java/io/github/cattslmao/bedrockparity/BedrockParityClient.java
+++ src/client/java/io/github/cattslmao/bedrockparity/BedrockParityClient.java
@@ -1,7 +1,10 @@
 package io.github.cattslmao.bedrockparity;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+@Environment(EnvType.CLIENT)
 public class BedrockParityClient implements ClientModInitializer {
 	public static final ParityConfig config = new ParityConfig("client");
 
